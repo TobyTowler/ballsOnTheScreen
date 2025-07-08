ab78288c61be2fdadcd887f3c3396bc76887602c
Toby Towler <tobytowler11@gmail.com>
2025-07-08 01:37:05 +0100

    cleanup

--- build/CMakeFiles/Interpreter.dir/src/balls.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Interpreter.dir/src/balls.cpp.o: \
- /home/toby/Programming/CPP/ballsOnTheScreen/src/balls.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/Interpreter.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Interpreter.dir/src/main.cpp.o: \
- /home/toby/Programming/CPP/ballsOnTheScreen/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/toby/Programming/CPP/ballsOnTheScreen/include/gui.h
